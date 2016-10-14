97b90446f90162d5e0d6e72595da8cab150f0d6a
Leonardo Gregianin <leogregianin@gmail.com>
2016-10-13 22:51:15 -0400

    Change character set

--- html_generator.py
+++ html_generator.py
@@ -6,7 +6,7 @@ main_page_head = '''
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
     <title>Filmes para assistir</title>
 
     <!-- Bootstrap 3 -->
