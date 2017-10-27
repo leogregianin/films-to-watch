027302d001d3331785a1bf21b8dca7a383c72d43
Leonardo Gregianin <leogregianin@gmail.com>
2017-10-27 20:43:37 -0300

    update

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,17 +2,22 @@ import media
 import html_generator
 import json
 
-data = json.load(open('films.json'))
-movies = []
+def generator():
+	
+	data = json.load(open('films.json'))
+	movies = []
 
-for i in range(len(data["films"])):
+	for i in range(len(data["films"])):
 
-	data["films"][i]["key"] = media.Movie(
-		data["films"][i]["title"],
-		data["films"][i]["storyline"],
-		data["films"][i]["poster_image_url"],
-		data["films"][i]["trailer_youtube_url"])
+		data["films"][i]["key"] = media.Movie(
+			data["films"][i]["title"],
+			data["films"][i]["storyline"],
+			data["films"][i]["poster_image_url"],
+			data["films"][i]["trailer_youtube_url"])
 
-	movies.append(data["films"][i]["key"])
+		movies.append(data["films"][i]["key"])
 
-html_generator.open_movies_page(movies)
+	html_generator.open_movies_page(movies)
+
+if __name__ == '__main__':
+	generator()
