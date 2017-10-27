18f4a136eb637a67e9ea59533c55f4bdc21a3ac9
Leonardo Gregianin <leogregianin@gmail.com>
2017-10-27 12:03:02 -0300

    create json file to load films

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,20 +1,18 @@
 import media
 import html_generator
+import json
 
-sal_terra = media.Movie("O Sal da Terra",
-	"O fotógrafo brasileiro Sebastião Salgado viaja a 40 anos através dos continentes aos passos de uma humanidade sempre em mutação. O filme retrata esta história. Uma homenagem à beleza do planeta.",
-	"https://raw.githubusercontent.com/leogregianin/films-to-watch/master/poster/sal-terra.jpg?token=ABmyVfW2WjKPjsbPX0gzarr7PhCRqShOks5YCXskwA%3D%3D",
-	"https://www.youtube.com/watch?v=dT7wv2HSkYE")
+data = json.load(open('films.json'))
+movies = []
 
-ensaio_cegueira = media.Movie("Ensaio sobre a Cegueira",
-	"Filme baseado no livro de mesmo nome do autor português José Saramago.",
-	"https://raw.githubusercontent.com/leogregianin/films-to-watch/master/poster/cegueira.jpg?token=ABmyVeck95D7CCf0QaDT4emLTdUV7Dqrks5YCXsLwA%3D%3D",
-	"https://www.youtube.com/watch?v=AyYZA7bLsYA")
+for i in range(len(data["films"])):
 
-sobral_pinto = media.Movie("Sobral: O Homem que não tinha Preço",
-	"Documentário sobre a história da vida de um dos mais emblemáticos advogados brasileiros Sobral Pinto.",
-	"https://raw.githubusercontent.com/leogregianin/films-to-watch/master/poster/sobral.jpg?token=ABmyVUEXEplYDtxUzYsl6pYk2kQEey5uks5YCXs4wA%3D%3D",
-	"https://www.youtube.com/watch?v=xvU4NGJ7CMk")
+	data["films"][i]["key"] = media.Movie(
+		data["films"][i]["title"],
+		data["films"][i]["storyline"],
+		data["films"][i]["poster_image_url"],
+		data["films"][i]["trailer_youtube_url"])
 
-movies = [sal_terra, ensaio_cegueira, sobral_pinto]
+	movies.append(data["films"][i]["key"])
+
 html_generator.open_movies_page(movies)
