ba16d757c9694d9b76e37f6e821b8857dc5833d6
Leonardo Gregianin <leogregianin@gmail.com>
2017-10-27 21:10:21 -0300

    separate js and css files

--- html_generator.py
+++ html_generator.py
@@ -1,6 +1,74 @@
 import os
 import re
 
+js_functions = '''
+// Pause the video when the modal is closed
+$(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+    // Remove the src so the player itself gets removed, as this is the only
+    // reliable way to ensure the video stops playing in IE
+    $("#trailer-video-container").empty();
+});
+// Start playing the video whenever the trailer modal is opened
+$(document).on('click', '.movie-tile', function (event) {
+    var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+    var sourceUrl = 'https://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+    $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+      'id': 'trailer-video',
+      'type': 'text-html',
+      'src': sourceUrl,
+      'frameborder': 0
+    }));
+});
+// Animate in the movies when the page loads
+$(document).ready(function () {
+  $('.movie-tile').hide().first().show("fast", function showNext() {
+    $(this).next("div").show("fast", showNext);
+  });
+});
+'''
+
+css_style = '''
+body {
+    padding-top: 80px;
+}
+#trailer .modal-dialog {
+    margin-top: 200px;
+    width: 640px;
+    height: 480px;
+}
+.hanging-close {
+    position: absolute;
+    top: -12px;
+    right: -12px;
+    z-index: 9001;
+}
+#trailer-video {
+    width: 100%;
+    height: 100%;
+}
+.movie-tile {
+    margin-bottom: 20px;
+    padding-top: 20px;
+}
+.movie-tile:hover {
+    background-color: #EEE;
+    cursor: pointer;
+}
+.scale-media {
+    padding-bottom: 56.25%;
+    position: relative;
+}
+.scale-media iframe {
+    border: none;
+    height: 100%;
+    position: absolute;
+    width: 100%;
+    left: 0;
+    top: 0;
+    background-color: white;
+}
+'''
+
 main_page_head = '''
 <!DOCTYPE html>
 <html lang="en">
@@ -9,77 +77,13 @@ main_page_head = '''
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>Films to watch</title>
 
-    <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <style type="text/css" media="screen">
-        body {
-            padding-top: 80px;
-        }
-        #trailer .modal-dialog {
-            margin-top: 200px;
-            width: 640px;
-            height: 480px;
-        }
-        .hanging-close {
-            position: absolute;
-            top: -12px;
-            right: -12px;
-            z-index: 9001;
-        }
-        #trailer-video {
-            width: 100%;
-            height: 100%;
-        }
-        .movie-tile {
-            margin-bottom: 20px;
-            padding-top: 20px;
-        }
-        .movie-tile:hover {
-            background-color: #EEE;
-            cursor: pointer;
-        }
-        .scale-media {
-            padding-bottom: 56.25%;
-            position: relative;
-        }
-        .scale-media iframe {
-            border: none;
-            height: 100%;
-            position: absolute;
-            width: 100%;
-            left: 0;
-            top: 0;
-            background-color: white;
-        }
-    </style>
-    <script type="text/javascript" charset="utf-8">
-        // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-            // Remove the src so the player itself gets removed, as this is the only
-            // reliable way to ensure the video stops playing in IE
-            $("#trailer-video-container").empty();
-        });
-        // Start playing the video whenever the trailer modal is opened
-        $(document).on('click', '.movie-tile', function (event) {
-            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'https://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-              'id': 'trailer-video',
-              'type': 'text-html',
-              'src': sourceUrl,
-              'frameborder': 0
-            }));
-        });
-        // Animate in the movies when the page loads
-        $(document).ready(function () {
-          $('.movie-tile').hide().first().show("fast", function showNext() {
-            $(this).next("div").show("fast", showNext);
-          });
-        });
-    </script>
+    <!-- <style type="text/css" media="screen"></style> -->
+    <link rel="stylesheet" href="css/films.css">
+    <script src="js/films.js"></script>
 </head>
 '''
 
@@ -145,13 +149,24 @@ def create_movie_tiles_content(movies):
 
 
 def open_movies_page(movies):
-    # Create or overwrite the output file
+    
+    # Create or overwrite the output js file
+    js_file = open('js/films.js', 'w')
+    js_file.write(js_functions)
+    js_file.close()
+
+    # Create or overwrite the output css file
+    css_file = open('css/films.css', 'w')
+    css_file.write(css_style)
+    css_file.close()
+
+    # Create or overwrite the output html file
     output_file = open('index.html', 'w')
 
     # Replace the movie tiles placeholder generated content
     rendered_content = main_page_content.format(
         movie_tiles=create_movie_tiles_content(movies))
 
-    # Output the file
+    # Output the html file
     output_file.write(main_page_head + rendered_content)
     output_file.close()
